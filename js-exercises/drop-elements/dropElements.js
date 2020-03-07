9660328bdb9108c3596957f502d3522e7b8ee6eb
Tabrez Khan <khan.t.29@gmail.com>
2020-03-07 16:04:40 +0530

    Solve drop-elements exercise

--- js-exercises/drop-elements/dropElements.js
+++ js-exercises/drop-elements/dropElements.js
@@ -1,3 +1,12 @@
-function dropElements(elements, predicate) {}
+function dropElements(elements, predicate) {
+  // for (let i = 0; i < elements.length; i += 1) {
+  //   if (!predicate(elements[i])) {
+  //     elements.splice(i, 1);
+  //   }
+  // }
+  // console.log(elements);
+  // return elements;
+  return elements.filter(predicate);
+}
 
 export { dropElements };
