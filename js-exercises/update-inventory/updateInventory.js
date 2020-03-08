37e23c7644d80a0671eaf082893d73eb965fc2e1
Tabrez Khan <khan.t.29@gmail.com>
2020-03-08 11:07:34 +0530

    Solve update-inventory exercise

--- js-exercises/update-inventory/updateInventory.js
+++ js-exercises/update-inventory/updateInventory.js
@@ -1,7 +1,30 @@
-function updateInventory(...args) {
-  return args;
+function updateInventory(currentInventory, newInventory) {
+  for (let i = 0; i < newInventory.length; i += 1) {
+    let itemFound = false;
+    const newItem = newInventory[i];
+    const [newItemQuantity, newItemName] = newItem;
+    for (let j = 0; j < currentInventory.length; j += 1) {
+      const currentItem = currentInventory[j];
+      const [currentItemQuantity, currentItemName] = currentItem;
+      if (newItemName === currentItemName) {
+        itemFound = true;
+        const updatedItem = [
+          currentItemQuantity + newItemQuantity,
+          currentItemName
+        ];
+        currentInventory[j] = updatedItem;
+        break;
+      }
+    }
+    if (!itemFound) {
+      currentInventory.push(newItem);
+    }
+  }
+  return currentInventory.sort((a, b) => {
+    if (a[1] < b[1]) return -1;
+    if (a[1] > b[1]) return 1;
+    return 0;
+  });
 }
 
-export {
-  updateInventory,
-};
+export { updateInventory };
