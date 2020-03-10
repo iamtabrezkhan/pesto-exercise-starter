dd8de2533ab64eda1631ea9227b17810943fe51e
Tabrez Khan <khan.t.29@gmail.com>
2020-03-10 15:36:07 +0530

    Update is-triangle
    More readable code

--- js-exercises/is-triangle/isTriangle.js
+++ js-exercises/is-triangle/isTriangle.js
@@ -1,8 +1,7 @@
-function isTriangle(a, b, c) {
-  if (a + b > c && a + c > b && b + c > a) {
-    return true;
-  }
-  return false;
+function isTriangle(side1, side2, side3) {
+  return (
+    side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1
+  );
 }
 
 export { isTriangle };
