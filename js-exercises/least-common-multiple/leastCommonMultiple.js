890fe001febf51d1b2b94eb4f9a8f7d65442bca1
Tabrez Khan <khan.t.29@gmail.com>
2020-03-13 16:55:22 +0530

    Write test cases for least-common-multiple problem, solve least-common-multiple problem

--- js-exercises/least-common-multiple/leastCommonMultiple.js
+++ js-exercises/least-common-multiple/leastCommonMultiple.js
@@ -1,8 +1,24 @@
-
-function leastCommonMultiple(...args) {
-  return args;
+function gcd(a, b) {
+  let t;
+  while (b !== 0) {
+    t = b;
+    b = a % b;
+    a = t;
+  }
+  return a;
 }
 
-export {
-  leastCommonMultiple,
-};
+function leastCommonMultiple(a, b) {
+  a = parseInt(a);
+  b = parseInt(b);
+  if (Number.isNaN(a)) {
+    throw new TypeError(`Expected number, received ${typeof a}`);
+  }
+  if (Number.isNaN(b)) {
+    throw new TypeError(`Expected number, received ${typeof b}`);
+  }
+  const lcm = (a * b) / gcd(a, b);
+  return lcm;
+}
+
+export { leastCommonMultiple };
