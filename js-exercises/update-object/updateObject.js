bc5a90f685f25d2a0ea0c1720c2ffa322e537780
Tabrez Khan <khan.t.29@gmail.com>
2020-03-13 14:32:35 +0530

    Write test cases for update-object problem, solve update-object problem

--- js-exercises/update-object/updateObject.js
+++ js-exercises/update-object/updateObject.js
@@ -1,8 +1,10 @@
-
-function updateObject(...args) {
-  return args;
+function updateObject(index, replaceWith, array) {
+  if (!(typeof index === "number")) {
+    throw new TypeError();
+  }
+  const clonedArray = array.slice();
+  clonedArray.splice(index, 1, replaceWith);
+  return clonedArray;
 }
 
-export {
-  updateObject,
-};
+export { updateObject };
--- js-exercises/update-object/updateObject.test.js
+++ js-exercises/update-object/updateObject.test.js
@@ -1,3 +1,18 @@
 import { updateObject } from "./updateObject";
 
-describe("updateObject", () => {});
+describe("updateObject", () => {
+  test("should return correct array", () => {
+    expect(updateObject(1, 10, [1, 2, 3, 4])).toEqual([1, 10, 3, 4]);
+  });
+  test("should replace dog with cat on 3rd index in an array", () => {
+    expect(
+      updateObject(3, "cat", ["house", "lion", "fish", "dog", "hotdog"])
+    ).toEqual(["house", "lion", "fish", "cat", "hotdog"]);
+  });
+  test("should throw an error if 1st argument (index) was not a number", () => {
+    expect(() => updateObject("boo", 10, [1, 2, 3, 4])).toThrow();
+  });
+  test("should throw an error if 3rd argument (array) was not an array", () => {
+    expect(() => updateObject(2, 10, {})).toThrow();
+  });
+});
