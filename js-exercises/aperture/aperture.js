2a8531a7b50fc4c3b526d4d509a13d43e50534d5
Tabrez Khan <khan.t.29@gmail.com>
2020-03-15 17:27:15 +0530

    Add edge cases in aperture function to throw TypeError

--- js-exercises/aperture/aperture.js
+++ js-exercises/aperture/aperture.js
@@ -1,4 +1,14 @@
 function aperture(n, array) {
+  if (typeof n !== "number") {
+    throw new TypeError(
+      `Expected 1st argument of type [number], received ${typeof n}`
+    );
+  }
+  if (!Array.isArray(array)) {
+    throw new TypeError(
+      `Expected 2nd argument of type [array], received ${typeof array}`
+    );
+  }
   if (n > array.length) {
     return [];
   }
